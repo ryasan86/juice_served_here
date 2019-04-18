bdd9f127c98891cee22c2594cc61cef92d61f48e
Ryan Santos <ryansantos86@gmail.com>
2019-04-17 22:12:55 -0700

    hero

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,14 @@ module.exports = {
         test: /\.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
+      {
+        test: /\.(png|jpg|gif|svg|ico)$/,
+        use: [
+          {
+            loader: 'file-loader?name=[name].[ext]', // retain original file name
+          },
+        ],
+      },
     ],
   },
 };
