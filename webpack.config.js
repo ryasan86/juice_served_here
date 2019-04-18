9fc2e4eda5a9b3d414de792e930e9f250cf045b4
Ryan Santos <ryansantos86@gmail.com>
2019-04-18 13:31:17 -0700

    responsive hero

--- webpack.config.js
+++ webpack.config.js
@@ -3,20 +3,20 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'main.js', // js bundle
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: ['style-loader', 'css-loader', 'sass-loader'], // sass
       },
       {
         test: /\.(png|jpg|gif|svg|ico)$/,
         use: [
           {
-            loader: 'file-loader?name=[name].[ext]', // retain original file name
+            loader: 'file-loader?name=assets/images/[name].[ext]', // output images to assets folder
           },
         ],
       },
