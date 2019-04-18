f42cbf934a9a8ad328d0b8e3e790d6291900ee76
Ryan Santos <ryansantos86@gmail.com>
2019-04-18 14:01:20 -0700

    starting js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './styles/index.scss';
+
+const navItems = document.querySelectorAll('.nav-item');
+
+// navItems.forEach((navItem, i) => {
+//   navItem.addEventListener('click', e => {
+//     navItem.classList.add('active');
+//   });
+// });
